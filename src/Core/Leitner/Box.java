65b29e54d20f535a7bc59ca73508bb42a8624801
wicked7000 <jason_attwood@hotmail.co.uk>
2017-03-10 12:23:09 +0000

    School Work ( FileSaver , Box and Unixtime)

--- src/Core/Leitner/Box.java
+++ src/Core/Leitner/Box.java
@@ -7,12 +7,42 @@ public class Box {
 
     public int BoxNumber;
     public ArrayList<Holder> Questions;
-    public int TimeToSee = 0;
+    public long TimeToSee = 0;
 
     public Box(int _BN, ArrayList<Holder> _Q, int _Time) {
         BoxNumber = _BN;
         Questions = _Q;
-        TimeToSee = _Time;
+        TimeToSee = System.currentTimeMillis() / 1000L + GetSpacing(_Time);
+    }
+
+    public long GetSpacing(int TimeConstant){
+        long Temp = 0;
+        switch(TimeConstant){
+            case -1:
+                Temp = 0;
+                break;
+            case 0:
+                //Add a hour!
+                Temp = 3600;
+                break;
+            case 1:
+                //Add a Day!
+                Temp = 86400; 
+                break;
+            case 3:
+                //Add a Week!
+                Temp = 604800;
+                break;
+            case 4:
+                //Add a month!
+                Temp = 2629743;
+                break;
+            case 5:
+                //Add a year!
+                Temp = 31556926;
+                break;
+        }
+        return Temp;
     }
 
     public void RemoveQuestion(Holder Question) {
--- src/Core/Leitner/Deck.java
+++ src/Core/Leitner/Deck.java
@@ -25,17 +25,23 @@ public class Deck {
     public void AddQuestion(String _Q, String _A) {
         Boxes[0].AddQuestion(_Q, _A);
     }
+    
+    //Document Function
+    public void AddQuestionHolder(Holder TheHolder){
+        Boxes[0].AddQuestionHolder(TheHolder);
+    }
+    
 
     public void AddBox(Box Y, int Number) {
         if (Boxes == null) {
-            Boxes = new Box[10];
+            Boxes = new Box[4];
         }
         Boxes[Number] = Y;
     }
 
     public void GenerateBoxes() {
-        for (int x = 0; x < 9; x++) {
-            Box _Temp = new Core.Leitner.Box(x, null, x * 5);
+        for (int x = 0; x < 4; x++) {
+            Box _Temp = new Core.Leitner.Box(x, null, x-1);
             AddBox(_Temp, x);
         }
     }
